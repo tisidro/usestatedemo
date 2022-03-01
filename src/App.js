3ade99e239f3371d0ae229512c72cb793f459aa2
Terri Isidro <terri.isidro@gmail.com>
2022-03-01 12:53:56 -0500

    update app.js file to include hard-coding for useState example

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React, { useState } from 'react'
+//--useState input hard-coded (will update/set state each time component re-render)
+// function App () {
+//   const [count, setCount] = useState(10) //sets initial state to 10
 
+//   function decrementCount () {
+//     setCount(prevCount => prevCount - 1)
+//   }
+
+//   function incrementCount () {
+//     setCount(prevCount => prevCount + 1)
+//   }
+//   return (
+//     <>
+//       <button onClick={decrementCount}>-</button>
+//       <span>{count}</span>
+//       <button onClick={incrementCount}>+</button>
+//     </>
+//   )
+// }
+
+//--useState input as function (will only update/set state on component's first re-render)
 // function App () {
 //   const [count, setCount] = useState(() => {
 //     return 10
